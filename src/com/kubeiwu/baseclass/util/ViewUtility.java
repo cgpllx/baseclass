2c989c9312659e8d69305fde3cc09a9fed5ea58f
cgpllx <cgpllx1@qq.com>
2015-04-07 17:25:38 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

--- src/com/kubeiwu/baseclass/util/ViewUtility.java
+++ src/com/kubeiwu/baseclass/util/ViewUtility.java
@@ -1,43 +1,36 @@
 package com.kubeiwu.baseclass.util;
 
 import android.app.Activity;
-import android.util.SparseArray;
 import android.view.View;
+import android.view.View.OnClickListener;
 
-@SuppressWarnings("unchecked")
 public class ViewUtility {
 
+	@SuppressWarnings("unchecked")
 	public static <T extends View> T findViewById(View view, int id) {
 		return (T) view.findViewById(id);
 	}
 
+	@SuppressWarnings("unchecked")
 	public static <T extends View> T findViewById(Activity activity, int id) {
 		return (T) activity.findViewById(id);
 	}
 
-	// 3、ListView使用ViewHolder极简写法
-	public static <T extends View> T getAdapterView(View convertView, int id) {
-		SparseArray<View> viewHolder = (SparseArray<View>) convertView.getTag();
-		if (viewHolder == null) {
-			viewHolder = new SparseArray<View>();
-			convertView.setTag(viewHolder);
+	@SuppressWarnings("unchecked")
+	public static <T extends View> T findViewById(View view, int id, OnClickListener onClickListener) {
+		T t = (T) view.findViewById(id);
+		if (onClickListener != null) {
+			t.setOnClickListener(onClickListener);
 		}
-		View childView = viewHolder.get(id);
-		if (childView == null) {
-			childView = convertView.findViewById(id);
-			viewHolder.put(id, childView);
-		}
-		return (T) childView;
+		return t;
 	}
-	// 用法：
-	// @Override
-	// public View getView(int position, View convertView, ViewGroup parent) {
-	// if (convertView == null) {
-	// convertView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_feed_item, parent, false);
-	// }
-	//
-	// ImageView thumnailView = getAdapterView(convertView, R.id.video_thumbnail);
-	// ImageView avatarView = getAdapterView(convertView, R.id.user_avatar);
-	// ImageView appIconView = getAdapterView(convertView, R.id.app_icon);
-	// }
+
+	@SuppressWarnings("unchecked")
+	public static <T extends View> T findViewById(Activity activity, int id, OnClickListener onClickListener) {
+		T t = (T) activity.findViewById(id);
+		if (onClickListener != null) {
+			t.setOnClickListener(onClickListener);
+		}
+		return t;
+	}
 }
