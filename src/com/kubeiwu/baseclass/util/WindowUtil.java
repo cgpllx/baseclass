42158bb94b6d8c613d2654bbce863e49dc0f0337
cgpllx <cgpllx1@qq.com>
2015-04-08 09:37:00 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

--- src/com/kubeiwu/baseclass/util/WindowUtil.java
+++ src/com/kubeiwu/baseclass/util/WindowUtil.java
@@ -2,6 +2,7 @@ package com.kubeiwu.baseclass.util;
 
 import android.content.Context;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.WindowManager;
 
 public class WindowUtil {
@@ -16,5 +17,18 @@ public class WindowUtil {
 		DisplayMetrics dm = new DisplayMetrics();
 		windowManager.getDefaultDisplay().getMetrics(dm);
 		return dm;
+	}
+	/**
+	 * 获取当前view的绝对坐标
+	 * @param view
+	 * @return
+	 */
+	public static int[] getViewInTheWindowPosition(View view) {
+		int[] location = new int[2];
+		if (view != null && view.getTag() != null) {
+			view.getLocationInWindow(location); // 获取在当前窗口内的绝对坐标
+			view.getLocationOnScreen(location);// 获取在整个屏幕内的绝对坐标
+		}
+		return location;
 	}
 }
